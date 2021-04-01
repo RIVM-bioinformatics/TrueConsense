88e713a815012dd08159411d85221547581dc13d
Florian Zwagemaker <florian@niqex.com>
2021-04-01 14:26:10 +0200

    formatting of coverage functions

--- TrueConsense/Coverage.py
+++ TrueConsense/Coverage.py
@@ -1,11 +1,10 @@
-
-
 def BuildCoverage(iDict, output):
     with open(output, "w") as outfile:
         for i in range(len(iDict)):
-            cov = iDict[i+1].get('coverage')
-            outfile.write(str(i+1) + "\t" + str(cov) + "\n")
+            cov = iDict[i + 1].get("coverage")
+            outfile.write(str(i + 1) + "\t" + str(cov) + "\n")
     pass
+
 
 def GetCoverage(iDict, position):
     return iDict[position].get("coverage")
