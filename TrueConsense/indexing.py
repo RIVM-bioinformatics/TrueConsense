fb64a5cfb85f0d83473be553c58ba9ce392cd412
Florian Zwagemaker <florian@niqex.com>
2021-04-01 14:25:43 +0200

    formatting of indexing functions

--- TrueConsense/indexing.py
+++ TrueConsense/indexing.py
@@ -5,26 +5,34 @@ import pysam
 
 
 def Readbam(f):
-    return pysam.AlignmentFile(f, 'rb')
+    return pysam.AlignmentFile(f, "rb")
+
 
 def Gffindex(file):
     return gffpd.read_gff3(file)
 
+
 def BuildIndex(bamfile, ref):
-    columns = ['coverage', 'A', 'T', 'C', 'G', 'X', 'I']
+    columns = ["coverage", "A", "T", "C", "G", "X", "I"]
     p_index = pd.DataFrame(columns=columns)
-    
+
     for r in pysamstats.stat_pileup(
-        type='variation',
+        type="variation",
         alignmentfile=Readbam(bamfile),
-        stepper='nofilter',
+        stepper="nofilter",
         fafile=ref,
         pad=True,
         one_based=True,
         max_depth=1000000000,
     ):
-        p_index.loc[r['pos']] = (
-            [r['reads_all']] + [r['A']] + [r['T']] + [r['C']] + [r['G']] + [r['deletions']] + [r['insertions']]
+        p_index.loc[r["pos"]] = (
+            [r["reads_all"]]
+            + [r["A"]]
+            + [r["T"]]
+            + [r["C"]]
+            + [r["G"]]
+            + [r["deletions"]]
+            + [r["insertions"]]
         )
-        
+
     return p_index
